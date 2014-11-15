fedef64379a1c828485edf95becbec69fe4f94ea
Jay Hack <jhack@stanford.edu>
2014-11-15 15:49:39 -0800

    trying to push to sciencebox

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -27,7 +27,18 @@ Fall 2014
 ##################
 """
 import os
-import pandas
+import pandas as pd
+
+
+def parse_quarter_dirname(quarter_dir):
+	"""
+		given AERS directory name, returns 
+			year, quarter
+	"""
+	quarter_dir = os.path.split(quarter_dir)[-1]
+	year = int(quarter_dir[:4])
+	quarter = int(quarter_dir[5])
+	return year, quarter
 
 
 def parse_filename(filepath):
@@ -36,11 +47,17 @@ def parse_filename(filepath):
 			file_descriptor, year, quarter
 	"""
 	filename = os.path.split(filepath)[-1]
-	file_descriptor = filename[:4].lower()
+	file_descriptor = filename[:4].upper()
 	year = int(filename[4:6])
 	quarter = int(filename[7])
 	return file_descriptor, year, quarter
 
+
+def parse_drug_ind_ther(df):
+	"""
+		does groupby to put all drugs in same row 
+	"""
+	df.groupby()
 
 
 def parse_ascii_directory(path):
@@ -58,6 +75,7 @@ def parse_ascii_directory(path):
 	data_paths = [os.path.join(path, p) for p in os.listdir(path) if p.endswith('.txt')]
 	with click.progressbar(data_paths) as _data_paths:
 		for p in _data_paths:
+			print ' : %s' % p
 			file_descriptor, year, quarter = parse_filename(p)
 			dfs[file_descriptor] = pd.read_csv(open(p, 'r'), delimiter='$')
 
@@ -78,11 +96,19 @@ import click
 @click.option('-o', '--output', help='Input directory containing ascii files')
 def preprocess(input_dir, output):
 
-	#=====[ Step 1: get individual dataframes ]=====
-	df = parse_ascii_directory(input_dir)
+	data_dir = input_dir
+
+	#=====[ Step 1: For each quarter... ]=====
+	quarter_dir = [os.path.join(data_dir, p) for p in os.listdir(input_dir)]
+	for quarter_dir in quarter_dirs:
+
+		#=====[ Step 2: parse 	]=====
+		year, quarter = parse_quarter_dirname(quarter_dir)
+		print '%s: %d, %d' % (quarter_dir, year, quarter)
+
 
 
 
 
 if __name__ == '__main__':
-	df = preprocess()
+	dfs = preprocess()
