dfa85ea3164dde0d7474032b4fc259bc2e0a93f3
Jay Hack <jhack@stanford.edu>
2014-11-15 13:11:36 -0800

    first commit

---
+++ configure.sh
@@ -0,0 +1,2 @@
+export PROJECT_DIR=/Users/jayhack/CS/DS/scikit_hammertime
+export PYTHONPATH=$PYTHONPATH:$PROJECT_DIR
---
+++ scripts/example_script.py
@@ -0,0 +1,26 @@
+"""
+Script: example_script
+======================
+
+Description:
+------------
+
+Arguments:
+----------
+
+Usage:
+------
+
+##################
+Jay Hack
+jhack@stanford.edu
+Fall 2014
+##################
+"""
+import click
+@click.command()
+def example_script():
+	pass 
+
+if __name__ == '__main__':
+	example_script()
