5327d195c9ce703fb6f2d7ddb91dc5365f614fcc
Jay Hack <jhack@stanford.edu>
2014-11-15 14:10:13 -0800

    added filename parsing function

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -29,6 +29,38 @@ Fall 2014
 import os
 import pandas
 
+def parse_filename(filename):
+	"""
+		given a filename, returns 
+			file_descriptor, year, quarter
+	"""
+	file_descriptor = filename[:4].lower()
+	year = int(filename[4:6])
+	quarter = int(filename[7])
+	return file_descriptor, year, quarter
+
+
+
+def parse_ascii_directory(path):
+	"""
+		given a path to a directory containing data files, 
+		(i.e. data/faers_ascii_2014q1)
+		returns a dataframe containing all the information
+	"""
+	#=====[ Step 1: ascii dir	]=====
+	path = os.path.join(path, 'ascii')
+	assert os.path.exists(path)
+
+	#=====[ Step 2: data files	]=====
+	data_paths = [os.path.join(path, p) for p in os.listdir(path) if p.endswith('.txt')]
+	for p in data_paths:
+		df = pd.read_csv(p, delimiter='$')
+
+
+
+
+
+
 import click
 @click.command()
 @click.option('-i', '--input_dir', help='Input directory containing ascii files', default=os.environ['DATA_DIR'])
