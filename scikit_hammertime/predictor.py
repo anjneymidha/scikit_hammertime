3a05aff2c004e2c2750549c8adf5dabfeedc30ee
Jay Hack <jhack@stanford.edu>
2014-11-15 19:49:44 -0800

    branching off to predictor

--- scikit_hammertime/predictor.py
+++ scikit_hammertime/predictor.py
@@ -1,17 +1,98 @@
+"""
+Script: Predictor.py
+====================
+
+Description:
+------------
+
+    Class wrapping ML predictor for drug interactions
+
+##################
+Jay Hack
+jhack@stanford.edu
+Fall 2014
+##################
+"""
 import sklearn
 import pandas as pd
 import pickle as pkl
 
-class predictor(object):
-    def __init__(self, df_path):
-        self.df = pkl.load(open(df_path,'r'))
+class Predictor(object):
+    """
+        Class: Predictor 
+        ================
+
+        Class wrapping ML predictor for drug interactions
+    """
+    data_filename = 'data.df'
+    clf_filename = 'clf.pkl'
+
+
+    def __init__(self, data_dir='/data/aers/'):
+        """
+            data_dir: location of parameters 
+        """
+        self.load_data()
+        
+
+    ################################################################################
+    ####################[ INTERNALS  ]##############################################
+    ################################################################################
+
+    def load_data(self, name='traindata.df'):
+        """
+            loads data to train 
+        """
+        print '-----> Loading data'
+        data_path = os.path.join(self.data_dir, name)
+        if os.path.exists(data_path):
+            self.data = pkl.load(open(data_path, 'r'))
+        else:
+            self.data = None
+
+
+    def load_clf(self, name='classifier.pkl'):
+        """
+            loads the classifier 
+        """
+        print '-----> Loading clf'
+        clf_path = os.path.join(self.data_dir, name)
+        if os.path.exists(clf_path):
+            self.clf = pkl.load(open(clf_path))
+        else:
+            self.clf = None
+
+
+    def save_clf(self, name='classifier.pkl'):
+        """
+            saves the classifier to disk 
+        """
+        print '-----> Saving clf'
+        clf_path = os.path.join(self.data_dir, name)
+        pkl.dump(self.clf, open(clf_path, 'w'))
+
+
+
+    def train(self):
+        """
+            trains the classifier 
+        """
+        raise NotImplementedError
+
+
+    def predict(self, data):
+        """
+            returns p(interaction|data) for each possible type 
+            of interaction 
+        """
+        raise NotImplementedError
+
 
     def get_drugs(self):
         pass
 
     def get_conditions(self):
         pass
-
 
     def query(self, drugs, condition):
        pass 
